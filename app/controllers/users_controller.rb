de6b3391dd34ef290c131e497abc553a2531cc2f
Leandro <robotmanus@gmail.com>
2014-05-17 12:24:11 -0500

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+  
+
   def new
   	@user = User.new 
   end
@@ -9,6 +12,7 @@ class UsersController < ApplicationController
  def create
     @user = User.new(user_params)
     if @user.save
+        sign_in @user
         flash[:success] = "Welcome to the Sample App!"
         redirect_to @user
     else
@@ -22,6 +26,9 @@ private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+
+
 
 end
 
